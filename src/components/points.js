8cf6b297ed1f11c48db9ad936d73982897ec9053
Lucas <lucasmatiasblanco@outlook.com>
2022-12-08 13:26:32 -0300

    fixed highscore display

--- src/components/points.js
+++ src/components/points.js
@@ -1,11 +1,18 @@
-//import {useEffect} from "react"; 
+import {useState} from "react"; 
 
 function ScoreDisplay({status, highScore}) {
+
+        const [ maxHighScore, setMaxHighScore ] = useState(highScore); 
+
+        const displayHighScore = () => {
+            return highScore > maxHighScore ? setMaxHighScore(highScore) : maxHighScore
+        }
+
     
     return(
         
         <div className={`score-container ${status > 0 ? "positive" : "negative"} `}>
-            <h3>CURRENT SCORE: {status}</h3><h3>HIGHSCORE: {highScore}</h3>
+            <h3>CURRENT SCORE: {status}</h3><h3>HIGHSCORE: {displayHighScore()}</h3>
         </div>
     )
 }
