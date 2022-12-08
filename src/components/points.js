6d33a52286224b5f032750a442c1a22c530695c6
Lucas <lucasmatiasblanco@outlook.com>
2022-12-08 13:38:32 -0300

    some fixes to highScore

--- src/components/points.js
+++ src/components/points.js
@@ -1,18 +1,24 @@
-import {useState} from "react"; 
+import {useEffect, useState} from "react"; 
 
 function ScoreDisplay({status, highScore}) {
 
         const [ maxHighScore, setMaxHighScore ] = useState(highScore); 
 
-        const displayHighScore = () => {
-            return highScore > maxHighScore ? setMaxHighScore(highScore) : maxHighScore
-        }
+       
 
-    
+        useEffect(() => {
+            const displayHighScore = () => {
+                return highScore > maxHighScore ? setMaxHighScore(highScore) : maxHighScore
+            }
+         
+            displayHighScore(); 
+        }, [highScore, maxHighScore])
+
+
     return(
         
         <div className={`score-container ${status > 0 ? "positive" : "negative"} `}>
-            <h3>CURRENT SCORE: {status}</h3><h3>HIGHSCORE: {displayHighScore()}</h3>
+            <h3>CURRENT SCORE: {status}</h3><h3>HIGHSCORE: {maxHighScore}</h3>
         </div>
     )
 }
